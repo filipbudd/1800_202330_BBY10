9606f3b3bb0c4b0b69911f14fc45e4bca3fb4b9b
Evin   <evingonzales2001@gmail.com>
2023-11-01 12:24:15 -0700

    Got firebase auth up and working. I kinda own all the data atm

--- scripts/firebaseAPI_TEAM10.js
+++ scripts/firebaseAPI_TEAM10.js
@@ -1,9 +1,6 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-
+  //import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
   // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyADEMmN3UaQEu13pKp73zNaXWPYjob_hxk",
@@ -15,4 +12,4 @@
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const app = firebase.initializeApp(firebaseConfig);
