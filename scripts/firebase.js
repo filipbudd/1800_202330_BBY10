124ef9c6ffb1d698215a60e87599dc0e91e6c36c
Evin   <evingonzales2001@gmail.com>
2023-11-01 11:37:10 -0700

    added an incomplete firebase script. Don't touch

---
+++ scripts/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyADEMmN3UaQEu13pKp73zNaXWPYjob_hxk",
+    authDomain: "nearbynexus-b7719.firebaseapp.com",
+    projectId: "nearbynexus-b7719",
+    storageBucket: "nearbynexus-b7719.appspot.com",
+    messagingSenderId: "1001592887147",
+    appId: "1:1001592887147:web:1ecd90e207df930c401e2e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
