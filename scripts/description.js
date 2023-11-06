f85877248fb461e310e8cc3426531dbbb713524a
Evin   <evingonzales2001@gmail.com>
2023-11-06 12:28:27 -0800

    I did a lot of stuff for the dynamic page for the events. I think I reached a milestone so I just wanted to upload it

--- scripts/description.js
+++ scripts/description.js
@@ -7,7 +7,81 @@ function bookmark(){
     }
 }
 
-function addBookmark(){
-    var eventsRef = db.collection("events").;
-    var userRef = db.collection("users");
+function updateBookmark(){
+    db.collection("events").doc("CKMMGiUDrJKDjigW8aHP").update({
+        "bookmarked": true
+    }).then(function(){
+        console.log("this worked?");
+    });
 }
+
+
+
+
+//EVINS STUFF BELOW
+function displayHikeInfo() {
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+    console.log( ID );
+
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection( "events" )
+        .doc( ID )
+        .get()
+        .then( doc => {
+            thisHike = doc.data();
+            hikeCode = thisHike.code;
+            hikeName = doc.data().name;
+            
+            // only populate title, and image
+            document.getElementById( "hikeName" ).innerHTML = hikeName;
+            let imgEvent = document.querySelector( ".hike-img" );
+            imgEvent.src = "../images/" + hikeCode + ".jpg";
+        } );
+}
+displayHikeInfo();
+
+
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayPageDynamically(collection) {
+    let pageTemplate = document.getElementById("TEMPLATEDESC"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get("events")   //the collection called "hikes"
+        .then(allHikes=> {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allHikes.forEach(doc => { //iterate thru each doc
+                var title = doc.data().eventName;       
+                var details = doc.data().eventDescription;
+                var host = doc.data().eventHost;
+                var address = doc.data().eventLocation; //Might have to change this later
+                var price = doc.data().eventPrice;
+                var date = doc.data().eventDate;
+                let newpage = pageTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+
+                var docID = doc.id;
+
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID="+docID;
+
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("hikes");  //input param is the name of the collection
