ee0d53e7f06c71f49be3816f1b98f2a045c90463
Evin   <evingonzales2001@gmail.com>
2023-11-06 21:46:50 -0800

    Finally fixed the read to the website regarding event details. I just need to make it ID dynamic and it's good to go

--- scripts/description.js
+++ scripts/description.js
@@ -19,72 +19,63 @@ function addBookmark(){
 
 
 //EVINS STUFF BELOW
-function displayHikeInfo() {
-    let params = new URL( window.location.href ); //get URL of search bar
-    let ID = params.searchParams.get( "docID" ); //get value for key "id"
-    console.log( ID );
+// function displayHikeInfo() {
+//     let params = new URL( window.location.href ); //get URL of search bar
+//     let ID = params.searchParams.get( "docID" ); //get value for key "id"
+//     console.log( ID );
 
-    // doublecheck: is your collection called "Reviews" or "reviews"?
-    db.collection( "events" )
-        .doc( ID )
-        .get()
-        .then( doc => {
-            thisHike = doc.data();
-            hikeCode = thisHike.code;
-            hikeName = doc.data().name;
+//     // doublecheck: is your collection called "Reviews" or "reviews"?
+//     db.collection( "events" )
+//         .doc( ID )
+//         .get()
+//         .then( doc => {
+//             thisHike = doc.data();
+//             hikeCode = thisHike.code;
+//             hikeName = doc.data().name;
             
-            // only populate title, and image
-            document.getElementById( "hikeName" ).innerHTML = hikeName;
-            let imgEvent = document.querySelector( ".hike-img" );
-            imgEvent.src = "../images/" + hikeCode + ".jpg";
-        } );
-}
-displayHikeInfo();
+//             // only populate title, and image
+//             document.getElementById( "hikeName" ).innerHTML = hikeName;
+//             let imgEvent = document.querySelector( ".hike-img" );
+//             imgEvent.src = "../images/" + hikeCode + ".jpg";
+//         } );
+// }
+// displayHikeInfo();
 
 
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
 function displayPageDynamically(collection) {
-    let pageTemplate = document.getElementById("TEMPLATEDESC"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
 
-    db.collection(collection).get("events")   //the collection called "hikes"
-        .then(allHikes=> {
-            //var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allHikes.forEach(doc => { //iterate thru each doc
+
+
+    document.getElementById("event-description").innerHTML = "PLEASE WORK";
+
+
+    let pageDesc = document.getElementById("eventPage"); // Retrieve the HTML element with the ID "eventPage" and store it in the cardTemplate variable. 
+    console.log(pageDesc);
+
+    db.collection(collection).get("events")   
+        .then(allInfo=> {
+            
+            allInfo.forEach(doc => { 
                 var title = doc.data().eventName;       
                 var details = doc.data().eventDescription;
                 var host = doc.data().eventHost;
                 var address = doc.data().eventLocation; //Might have to change this later
                 var price = doc.data().eventPrice;
                 var date = doc.data().eventDate;
-                let newpage = pageTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                var priceStatus = doc.data().eventPriceStatus;
 
-
-                var docID = doc.id;
-
-
-                //update title and text and image
-                // newcard.querySelector('.card-title').innerHTML = title;
-                // newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
-                // newcard.querySelector('.card-text').innerHTML = details;
-                // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
-                // newcard.querySelector('a').href = "eachHike.html?docID="+docID;
-
-
-                newpage.querySelector('.event-description').innerHTML = details;
-
-                //Optional: give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-
-                //attach to gallery, Example: "hikes-go-here"
-                document.getElementById(collection + "-go-here").appendChild(newcard);
-
-                //i++;   //Optional: iterate variable to serve as unique ID
+                document.getElementById("event-title").innerHTML = title;
+                document.getElementById("event-description").innerHTML = details;
+                document.getElementById("event-cost").innerHTML = price;
+                document.getElementById("price-status").innerHTML = priceStatus;
+                document.getElementById("event-date").innerHTML = date;
+                document.getElementById("event-address").innerHTML = address;
+                document.getElementById("event-host").innerHTML = host;
             })
         })
 }
 
-displayPageDynamically("events");  //input param is the name of the collection=
+displayPageDynamically("events");  
