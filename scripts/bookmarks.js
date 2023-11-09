2059c98e9649e858be1e4c8266604157fbfa89b7
Eugenie Kim <ekim138@my.bcit.ca>
2023-11-09 09:33:24 -0800

    editted all bookmarking related functions to fit the new way of storing bookmarks in an array

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -1,50 +1,50 @@
 function deleteBookmark(id){
+    var userID = "J2A4IddxuURAJsYt0cEn3Kc5Nx73";
     var bookmarksRef = db.collection("users")
-        .doc("J2A4IddxuURAJsYt0cEn3Kc5Nx73")
-        .collection("bookmarks"); //user collection should not be hardcoded
-    var bookmarkID = id
-
-    bookmarksRef.doc(bookmarkID).delete();
-
+        .doc(userID); //user collection should not be hardcoded
+    bookmarksRef.update({ bookmarks: firebase.firestore.FieldValue.arrayRemove(id)});
 }
 
 
-async function displayBookmarksDynamically(){
+function displayBookmarksDynamically(){
     let cardTemplate = document.getElementById("i_bookmark-template");
     
     //another thing to change to not be hard coded but user specfic
     var userID = "J2A4IddxuURAJsYt0cEn3Kc5Nx73"
-    var bookmarksRef = db.collection("users").doc(userID).collection("bookmarks");
-    var bookmarksSnapshotRef = await bookmarksRef.get();
+    var bookmarksRef = db.collection("users").doc(userID);
     
-    var bookmarkGoHere = document.getElementById("i_bookmarks-go-here")
-    if (bookmarksSnapshotRef.empty){
-        bookmarkGoHere.innerHTML = "You have no bookmarks";
-    } else {
-        bookmarksRef.get()
-        .then(allBookmarks=> {
-
-            allBookmarks.forEach(doc => {
-                var category = doc.data().eventInfo.eventCategory;
-                var title = doc.data().eventInfo.eventName;
-                var date = doc.data().eventInfo.eventDate;
-
+    var bookmarkGoHere = document.getElementById("i_bookmarks-go-here");
+    bookmarksRef.get().then((doc) => {
+        var bookmarksArray = doc.data().bookmarks;
+        if (bookmarksArray.length == 0){
+            bookmarkGoHere.innerHTML = "You have no bookmarks";
+        } else {
+            bookmarksArray.forEach(eventID => {
+                db.collection("events").doc(eventID).get().then(doc => {
+                    var category = doc.data().eventCategory;
+                var title = doc.data().eventName;
+                var date = doc.data().eventDate;
+    
                 var docID = doc.id;
-
+    
                 let newBookmark = cardTemplate.content.cloneNode(true);
-
+    
                 newBookmark.querySelector(".bookmark-category").innerHTML = category;
                 newBookmark.querySelector(".bookmark-title").innerHTML = title;
                 newBookmark.querySelector(".bookmark-date").innerHTML = date;
                 newBookmark.querySelector(".bookmark-btn").onClick = deleteBookmark(docID);
                 //gonna need to find how to reference specific hoke description to link that for the title
-
+    
                 bookmarkGoHere.appendChild(newBookmark);
-           
-            })
-        })
+                });
 
-    }
+                
+               
+                })
+        }
+    });
+
+    
 }
 
 displayBookmarksDynamically();
