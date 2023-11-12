4e899629d63ec954d37444a931c970b869b5d8de
Carol Li <lizhi.2496@gmail.com>
2023-11-11 21:47:30 -0800

    create the event submission form, with click function to the days.add a submission js file to save the value.

---
+++ scripts/submitEvent.js
@@ -0,0 +1,11 @@
+function saveSubmitInfo() {
+//a) get user entered values
+    category = document.getElementById("category-select").value;
+    duration = document.getElementById("multi-day-selected").value;
+    singleDay = document.getElementById("single-day-selected").value;
+    description = document.getElementById("description").value;
+    host = document.getElementById("inputHost").value;
+    location = document.getElementById("inputLocation").value;
+    title = document.getElementById("eventName").value;
+    cost = document.getElementById("cost").value;
+}
