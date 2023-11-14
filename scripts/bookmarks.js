b883ebb112e03450a9615415bafb0e97a0cd9d7d
Eugenie Kim <ekim138@my.bcit.ca>
2023-11-13 19:19:17 -0800

    got rid of all hardcoded values in bookmark related functions; made displayBookmarksDynamically correctly pull from events collections; added link on bookmarks in bookmarks page that led to the event details page; moved all functions from event.js to eachEvent.js and deleted event.js

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -20,25 +20,27 @@ function displayBookmarksDynamically() {
     var bookmarkGoHere = document.getElementById("i_bookmarks-go-here");
     bookmarksRef.get().then((doc) => {
         var bookmarksArray = doc.data().bookmarks;
+
         if (bookmarksArray.length == 0) {
             bookmarkGoHere.innerHTML = "You have no bookmarks";
         } else {
             bookmarksArray.forEach(eventID => {
                 db.collection("events").doc(eventID).get().then(doc => {
-                    var category = doc.data().eventCategory;
-                    var title = doc.data().eventName;
-                    var date = doc.data().eventDate;
+
+                    var category = doc.data().category;
+                    var title = doc.data().name;
+                    var date = doc.data().date;
 
                     var docID = doc.id;
 
                     let newBookmark = cardTemplate.content.cloneNode(true);
-
                     newBookmark.querySelector(".bookmark-category").innerHTML = category;
                     newBookmark.querySelector(".bookmark-title").innerHTML = title;
+                    newBookmark.querySelector(".bookmark-title").href = "eachEvent.html?docID=" + docID;
                     newBookmark.querySelector(".bookmark-date").innerHTML = date;
                     newBookmark.querySelector(".bookmark-btn").onclick = function () { deleteBookmark(docID); }
 
-                    //gonna need to find how to reference specific hoke description to link that for the title
+                    //gonna need to find how to reference specific hike description to link that for the title
 
                     bookmarkGoHere.appendChild(newBookmark);
                 });
--- scripts/eachEvent.js
+++ scripts/eachEvent.js
@@ -1,43 +1,81 @@
 //EVINS STUFF BELOW
 
+//Get the ID from the URL (look at the url)
+let params = new URL(window.location.href);
+var ID = params.searchParams.get("docID");
+
 function displayEventInfo() {
+    //Refer to the specific event
+    db.collection("events").doc(ID).get().then(doc => {
 
-  //Get the ID from the URL (look at the url)
-  let params = new URL(window.location.href);
-  let ID = params.searchParams.get("docID");
-  console.log(ID);
-  
-  //Refer to the specific event
-  db.collection( "events" ).doc(ID).get().then( doc => {
+        //Get all the necessary data from the DB and stores in variables
+        var ages = doc.data().ages;
+        var category = doc.data().category;
+        var address = doc.data().address;
+        var cost = doc.data().cost;
+        var date = doc.data().date
+        var description = doc.data().description;
+        var image = doc.data().image;
+        var link = doc.data().link;
+        var map = doc.data().map;
+        var name = doc.data().name;
+        var host = doc.data().host;
 
-          //Get all the necessary data from the DB and stores in variables
-          var ages = doc.data().ages;
-          var category = doc.data().category;
-          var address = doc.data().address;
-          var cost = doc.data().cost;
-          var date = doc.data().date
-          var description = doc.data().description;
-          var image = doc.data().image;
-          var link = doc.data().link;
-          var map = doc.data().map;
-          var name = doc.data().name;
-          var host = doc.data().host;
+        // populate all information, and image through the Ids
+        document.getElementById("event-name").innerHTML = name;
+        document.getElementById("category").innerHTML = category;
+        document.getElementById("cost").innerHTML = cost;
+        document.getElementById("date").innerHTML = date;
+        document.getElementById("address").innerHTML = address;
+        document.getElementById("host").innerHTML = host;
+        document.getElementById("description").innerHTML = description;
+        document.getElementById("ages").innerHTML = ages;
+        document.getElementById("link").href = "NOT_A_REAL_PAGE.html:) -Evin";
 
-          // populate all information, and image through the Ids
-          document.getElementById( "event-name" ).innerHTML = name;
-          document.getElementById("category").innerHTML = category;
-          document.getElementById("cost").innerHTML = cost;
-          document.getElementById("date").innerHTML = date;
-          document.getElementById("address").innerHTML = address;
-          document.getElementById("host").innerHTML = host;
-          document.getElementById("description").innerHTML = description;
-          document.getElementById("ages").innerHTML = ages;
-          document.getElementById("link").href = "NOT_A_REAL_PAGE.html:) -Evin";
-
-          //formats the image directory and adds it
-          let imgEvent = document.getElementById("bannerImg")
-          imgEvent.src = "../images/" + image + ".jpg";
-          console.log( ID + ' THIS IS THE DOC ID');
-  });
+        //formats the image directory and adds it
+        let imgEvent = document.getElementById("bannerImg")
+        imgEvent.src = "../images/" + image + ".jpg";
+        console.log(ID + ' THIS IS THE DOC ID');
+    });
 }
 displayEventInfo();
+
+
+
+
+
+//----------------------- BOOKMARK FUNCTIONS -----------------------//
+// Store the current user
+var userID = localStorage.getItem("user");
+console.log(userID);
+
+function bookmark() {
+    var bookmarksRef = db.collection("users").doc(userID);
+    bookmarksRef.onSnapshot((doc) => {
+        var bookmarksArray = doc.data().bookmarks;
+        console.log(bookmarksArray);
+        if (bookmarksArray.includes(ID)) {
+            document.getElementById("i_bookmark-icon").className = "material-icons";
+        } else {
+            document.getElementById("i_bookmark-icon").className = "material-symbols-outlined";
+        }
+    });
+}
+bookmark();
+
+
+function addBookmark() {
+
+    let icon = document.getElementById("i_bookmark-icon").className;
+    var bookmarksRef = db.collection("users").doc(userID);
+
+    if (icon == "material-symbols-outlined") {
+        bookmarksRef.update({
+            bookmarks: firebase.firestore.FieldValue.arrayUnion(ID)
+        });
+    } else {
+        bookmarksRef.update({
+            bookmarks: firebase.firestore.FieldValue.arrayRemove(ID)
+        });
+    }
+}
